996c192a411256884bc18dbf7649a2eaa22678d4
JunSeong Park <rasauq1122@gmail.com>
2019-11-28 23:41:53 +0900

    Add : Field(Class), Revise : Minion(Class)
    2019.11.28
    
    Field 클래스
    - 설명 : Minion 객체들을 보관함. 객체들의 명령 수행 및 이벤트 전달을 용이하게 할 예정
    - 메소드 :
    -- attack : 아군 하수인 하나로 적 하수인 하나를 공격하는 명령이 가능한 지 확인하고 수행
    -- minionDead : 하수인의 사망을 감지하여 적절한 조치를 취하고, 다른 하수인에게 이벤트 전달
    
    
    Minion 클래스
    - 메소드 :
    -- attack : 실제로 공격하는 행위, getDamage 메소드 호출
    -- getDamage : 들어온 인자 만큼 체력을 깎음, 사망 시 Field.minionDead 호출

--- Can-You-Beat/src/Minion.java
+++ Can-You-Beat/src/Minion.java
@@ -1,27 +1,49 @@
 public class Minion {
-	private int atk;
-	private int atkchc;
-	private int hel;
-	private int maxhel;
-	private boolean ally;
-	private String name;
-	
-	public Minion(String name, int atk, int maxhel, int hel, boolean ally)
-	{
-		this.name = name;
-		
-		if (atk < 0) atk = 0;
-		this.atk = atk;
-		
-		if (ally) atkchc = 1;
-		else atkchc = 0;
-		
-		if (hel < 1) hel = 1;
-		this.hel = hel;
+   private int atk;
+   private int atkchc;
+   private int hel;
+   private int maxhel;
+   private boolean ally;
+   private String name;
+   private Field myfield;
+   
+   public Minion(String name, int atk, int maxhel, int hel, boolean ally, Field myfield)
+   {
+      this.name = name;
+      
+      if (atk < 0) atk = 0;
+      this.atk = atk;
+      
+      if (ally) atkchc = 1;
+      else atkchc = 1;
+      
+      if (hel < 1) hel = 1;
+      this.hel = hel;
 
-		if (maxhel < hel) maxhel = hel;
-		this.maxhel = hel;
-		
-		this.ally = ally;
-	}
+      if (maxhel < hel) maxhel = hel;
+      this.maxhel = hel;
+      
+      this.ally = ally;
+      
+      this.myfield = myfield;
+   }
+   
+   public String getName() {return this.name;}
+   
+   public boolean canAttack() {return atkchc > 0;}
+   
+   public int getAttack() {return atk;}
+   
+   public void getDamage(int damage, Minion other)
+   {
+      hel -= other.getAttack();
+      if (hel <= 0) myfield.minionDead(this,ally);
+   }
+   
+   public void attack(Minion other)
+   {
+      atkchc--;
+      other.getDamage(this.getAttack(), this);
+      this.getDamage(other.getAttack(), other);
+   }
 }
